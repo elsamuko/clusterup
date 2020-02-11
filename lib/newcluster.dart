33fa7783ffef9db45babbfec9047fe62bea7e014
Samuel Albrecht <elsamuko@gmail.com>
2020-02-11 20:02:52 +0100

    add: connection test button

--- lib/newcluster.dart
+++ lib/newcluster.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:developer' as dev;
+import 'sshconnection.dart';
 import 'cluster.dart';
 
 class NewClusterState extends State<NewCluster> {
@@ -17,7 +19,7 @@ class NewClusterState extends State<NewCluster> {
             IconButton(
               icon: Icon(Icons.check),
               onPressed: () {
-                if( _formKey.currentState.validate() ) {
+                if (_formKey.currentState.validate()) {
                   _formKey.currentState.save();
                   dev.log("Saving new cluster ${_cluster}");
                   Navigator.pop(context, _cluster);
@@ -26,71 +28,96 @@ class NewClusterState extends State<NewCluster> {
             ),
           ],
         ),
-        body: Form(
-            key: _formKey,
-            child: Column(
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.label),
-                labelText: 'name',
-              ),
-              onSaved: (String value) {
-                  dev.log("Saving name");
-                  _cluster.name = value;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: 'username',
-                labelText: 'username',
-              ),
-              onSaved: (String value) {
-                setState(() {
-                  _cluster.user = value;
-                });
-              },
-              validator: (String value) {
-                return value.contains('@') ? 'Do not use the @ char.' : null;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.computer),
-                hintText: 'Server domain',
-                labelText: 'server',
-              ),
-              onSaved: (String value) {
-                _cluster.domain = value;
-              },
-              validator: (String value) {
-                return value.contains('@') ? 'Do not use the @ char.' : null;
-              },
-            ),
-            TextFormField(
-              initialValue: "22",
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.local_airport),
-                hintText: 'SSH server port',
-                labelText: 'port',
-              ),
-              onSaved: (String value) {
-                _cluster.port = int.parse(value);
-                // This optional block of code can be used to run
-                // code when the user saves the form.
-              },
-              validator: (String value) {
-                if (int.tryParse(value) == 0) {
-                  return "Invalid port number";
-                } else {
-                  return null;
-                }
-              },
-            ),
-          ],
-        )));
+        body: ListView(children: <Widget>[
+          Card(
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.label),
+                              labelText: 'name',
+                            ),
+                            onSaved: (String value) {
+                              _cluster.name = value;
+                            },
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.person),
+                              hintText: 'username',
+                              labelText: 'username',
+                            ),
+                            inputFormatters: [
+                              BlacklistingTextInputFormatter(RegExp("[ ]"))
+                            ],
+                            onSaved: (String value) {
+                              _cluster.user = value;
+                            },
+                            validator: (String value) {
+                              return value.contains('@')
+                                  ? 'Do not use the @ char.'
+                                  : null;
+                            },
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.computer),
+                              hintText: 'Server domain',
+                              labelText: 'server',
+                            ),
+                            inputFormatters: [
+                              BlacklistingTextInputFormatter(RegExp("[ ]"))
+                            ],
+                            onSaved: (String value) {
+                              _cluster.host = value;
+                            },
+                            validator: (String value) {
+                              return value.contains('@')
+                                  ? 'Do not use the @ char.'
+                                  : null;
+                            },
+                          ),
+                          TextFormField(
+                            initialValue: "22",
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.local_airport),
+                              hintText: 'SSH server port',
+                              labelText: 'port',
+                            ),
+                            onSaved: (String value) {
+                              _cluster.port = int.parse(value);
+                            },
+                            validator: (String value) {
+                              if (int.tryParse(value) == 0) {
+                                return "Invalid port number";
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                        ],
+                      )))),
+          Container(
+              margin: const EdgeInsets.all(20.0),
+              child: FlatButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      dev.log("Testing ${_cluster}");
+      bool ok = await SSHConnection.test(_cluster.user, _cluster.host, _cluster.port);
+    }
+                  },
+                  child: Text(
+                    "Test connection",
+                  ))),
+        ]));
   }
 }
 
