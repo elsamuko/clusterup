230cb94501e77535d25110c4132f16778e9ef6ab
Samuel Albrecht <elsamuko@gmail.com>
2020-02-16 10:26:38 +0100

    add: android and ios folders

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<ssh/SshPlugin.h>)
+#import <ssh/SshPlugin.h>
+#else
+@import ssh;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SshPlugin registerWithRegistrar:[registry registrarForPlugin:@"SshPlugin"]];
+}
+
+@end
