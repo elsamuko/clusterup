38b89f6613bf63a21e75779e63beac2c4e26abc1
Samuel Albrecht <elsamuko@gmail.com>
2020-02-16 10:21:24 +0100

    add: key class + test

---
+++ lib/key.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:pointycastle/export.dart';
+import 'package:pointycastle/pointycastle.dart';
+import 'rsa_key_helper.dart';
+
+class Key {
+  RSAPrivateKey _privateKey;
+  RSAPublicKey _publicKey;
+
+  Key(this._privateKey) {
+    BigInt publicExponent = RsaKeyHelper.getPublicExponent(_privateKey);
+    assert(publicExponent == BigInt.from(65537)); // usually it's 65537
+    _publicKey = RSAPublicKey(_privateKey.modulus, publicExponent);
+  }
+
+  // https://github.com/PointyCastle/pointycastle/blob/master/tutorials/rsa.md
+  static SecureRandom _getSecureRandom() {
+    final secureRandom = FortunaRandom();
+
+    final seedSource = Random.secure();
+    final seeds = <int>[];
+    for (int i = 0; i < 32; i++) {
+      seeds.add(seedSource.nextInt(255));
+    }
+    secureRandom.seed(KeyParameter(Uint8List.fromList(seeds)));
+
+    return secureRandom;
+  }
+
+  // https://github.com/dart-lang/sdk/issues/32803#issuecomment-387405784
+  List<int> _writeBigInt(BigInt number, [int size]) {
+    // Not handling negative numbers. Decide how you want to do that.
+    int bytes = size ?? 1 + (number.bitLength + 7) >> 3;
+    BigInt b256 = BigInt.from(256);
+    List<int> result = List<int>(bytes);
+
+    int pos = bytes - 1;
+
+    do {
+      result[pos] = number.remainder(b256).toInt();
+      number = number >> 8;
+    } while (pos-- != 0);
+
+    return result;
+  }
+
+  String pubForSSH() {
+    List<int> head = utf8.encode("ssh-rsa");
+    List<int> e = _writeBigInt(_publicKey.e);
+    List<int> n = _writeBigInt(_publicKey.n);
+    List<int> szHead = _writeBigInt(BigInt.from(head.length), 4);
+    List<int> szE = _writeBigInt(BigInt.from(e.length), 4);
+    List<int> szN = _writeBigInt(BigInt.from(n.length), 4);
+    List<int> binary = szHead + head + szE + e + szN + n;
+    return "ssh-rsa " + base64.encode(binary);
+  }
+
+  String privString() {
+    return RsaKeyHelper.encodePrivateKeyToPemPKCS1(_privateKey);
+  }
+
+  String pubString() {
+    return RsaKeyHelper.encodePublicKeyToPemPKCS1(_publicKey);
+  }
+
+  // https://github.com/PointyCastle/pointycastle/blob/master/tutorials/rsa.md
+  static Key generate() {
+    SecureRandom rnd = Key._getSecureRandom();
+
+    final rsaParams = RSAKeyGeneratorParameters(BigInt.from(65537), 4096, 64);
+    final params = ParametersWithRandom(rsaParams, rnd);
+
+    RSAKeyGenerator keyGenerator = RSAKeyGenerator();
+    keyGenerator.init(params);
+
+    final pair = keyGenerator.generateKeyPair();
+    final publicKey = pair.publicKey as RSAPublicKey;
+    final privateKey = pair.privateKey as RSAPrivateKey;
+
+    return Key(privateKey);
+  }
+
+  static Key fromPEM(String PEM) {
+    RSAPrivateKey privateKey = RsaKeyHelper.parsePrivateKeyFromPem(PEM);
+    return Key(privateKey);
+  }
+}
---
+++ test/key_test.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'package:clusterup/rsa_key_helper.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:clusterup/key.dart';
+import 'package:pointycastle/export.dart';
+
+void main() {
+  test('Key generation', () {
+    Key key = Key.generate();
+    String ssh = key.pubForSSH();
+    expect(ssh.isEmpty, false);
+  });
+
+  test('Key parsing', () {
+    // weak key for testing, use at least 2048 in production
+    // openssl genrsa -out private.pem 1024
+    String privPEM = """-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQCrtwUnVi/NlhO7TvqzJmlVJNAsU20EFmFijeS3QekYOqt+etCD
+7ZaAX8284orEeDJkxtYMC3NjCM23N/F/qEDE2QC4pdCuJP5+Ov/6mvy+6ZrxgIa1
++htTZMfpIKJZlvTiAkvQaFveivDfAQijrUbR+cWBp4loCCiLUC61Y51TQwIDAQAB
+AoGBAJ3Gn7SiK3AyGlU733xmqdfy6FgiG4Pq8HY2vFVp+TwrBFJFlHvz/RpdbNPG
+MA0QB/WzAQ+2IcJ4X1Se0YYjWcZDNr5LwtJpyyEgDozkAt2Ug+r7BrQCe2g3jOWU
+iJgiXVHGNiACfZIu6FFple22pJqhb461F9JAbeQt6rtDpRapAkEA3yRrqCWbpzD7
+baj/8raXZWNk28nHyIVC26Vi1hNY6CNWD20Z9v0TUSU22SJXeAXa+mHJKjs9j3bw
++2EH0cq7LwJBAMT//g8ssGlY9k8T6kza9bTsAqWW8NU85SFTi7BFHvKwP/4tyA/U
+mCG35T9Qc377iSyA4TFcvftQZyr0P8uhVC0CQQDVrUWeNa0w09ngb7XwkOK3Bw/c
+3AOAxAN624uindJEMRpHGV2Ew2FNEgrMsHL8DvdbTmpZE3NmvyoSPh9DyROnAkAL
+VRaOVOnJBZ8VqXWe+jGMOM9mKyqreZdMtXuhpjhDibQEsSmDD524wtVjMQOT2HBp
+qPhLWKRtIpDsvaQ12I/5AkEAzoq4e3Hin3hED9jsE7VPWINk/sTreDmMZTEdQyM9
+s/NAKRvWDv52+0iZRWxTRie1/DQ/4dfKo2R07uctJcdnbw==
+-----END RSA PRIVATE KEY-----""";
+
+    // openssl rsa -in private.pem -out public.pem -outform PEM -pubout
+    // openssl rsa -pubin -in public.pem -RSAPublicKey_out -out public.pkcs1
+    String pubPEM = """-----BEGIN RSA PUBLIC KEY-----
+MIGJAoGBAKu3BSdWL82WE7tO+rMmaVUk0CxTbQQWYWKN5LdB6Rg6q3560IPtloBf
+zbziisR4MmTG1gwLc2MIzbc38X+oQMTZALil0K4k/n46//qa/L7pmvGAhrX6G1Nk
+x+kgolmW9OICS9BoW96K8N8BCKOtRtH5xYGniWgIKItQLrVjnVNDAgMBAAE=
+-----END RSA PUBLIC KEY-----""";
+
+    // ssh-keygen -f public.pem -i -mPKCS8 > public.ssh
+    String expectedSSH =
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCrtwUnVi/NlhO7TvqzJmlVJNAsU20EFmFijeS3QekYOqt+etCD7ZaAX8284orEeDJkxtYMC3NjCM23N/F/qEDE2QC4pdCuJP5+Ov/6mvy+6ZrxgIa1+htTZMfpIKJZlvTiAkvQaFveivDfAQijrUbR+cWBp4loCCiLUC61Y51TQw==";
+
+    Key key = Key.fromPEM(privPEM);
+
+    String ssh = key.pubForSSH();
+    expect(ssh == expectedSSH, true);
+
+    String privGen = key.privString();
+    expect(privGen == privPEM, true);
+
+    String pubGen = key.pubString();
+    RSAPublicKey publicKey = RsaKeyHelper.parsePublicKeyFromPem(pubGen);
+    RSAPublicKey publicKey2 = RsaKeyHelper.parsePublicKeyFromPem(pubPEM);
+    expect(pubGen == pubPEM, true);
+  });
+}
