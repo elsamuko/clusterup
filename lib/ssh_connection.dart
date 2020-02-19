58e3db61f1018666d75f79eba3fcbd7f442e8927
Samuel Albrecht <elsamuko@gmail.com>
2020-02-19 23:13:10 +0100

    chg: return state and error message with SSHConnection

--- lib/ssh_connection.dart
+++ lib/ssh_connection.dart
@@ -4,15 +4,21 @@ import 'package:clusterup/ssh_key.dart';
 import 'package:ssh/ssh.dart';
 import 'package:flutter/services.dart';
 
+class SSHConnectionResult {
+  bool success;
+  String error;
+  SSHConnectionResult(this.success, [this.error]);
+}
+
 class SSHConnection {
-  static Future<bool> test(
+  static Future<SSHConnectionResult> test(
       String user, String host, int port, SSHKey key) async {
-    bool rv = false;
+    SSHConnectionResult rv = SSHConnectionResult(false);
     var client = new SSHClient(
       host: host,
       port: port,
       username: user,
-      passwordOrKey: {"privateKey": key.privString()},
+      passwordOrKey: {"privateKey": key?.privString() ?? ""},
     );
     if (client != null) {
       dev.log("trying to connect to $user@$host:$port");
@@ -21,9 +27,10 @@ class SSHConnection {
         dev.log("connected to $user@$host:$port");
         client.disconnect();
         dev.log("disconnected from $user@$host:$port");
-        rv = true;
+        rv.success = true;
       } on PlatformException catch (e) {
         print('Error: ${e.code}\nError Message: ${e.message}');
+        rv.error = e.message;
       }
     }
     return rv;
