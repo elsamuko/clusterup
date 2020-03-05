9c0cf7a4d2ac18b7fe6bce40d2874279af4e020d
Samuel Albrecht <elsamuko@gmail.com>
2020-03-05 23:22:17 +0100

    add: view for remote actions

---
+++ lib/remote_actions_view.dart
@@ -0,0 +1,90 @@
+import 'package:clusterup/remote_action.dart';
+import 'package:clusterup/ssh_key.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:developer' as dev;
+import 'ssh_connection.dart';
+import 'cluster.dart';
+
+class ActionsViewState extends State<ActionsView> {
+  ActionsViewState();
+
+  Widget _buildRow(RemoteAction action) {
+    final bool marked = widget._saved.contains(action);
+    IconButton leading = widget._selectable
+        ? IconButton(
+            icon:
+                Icon(marked ? Icons.check_box : Icons.check_box_outline_blank),
+            onPressed: () {
+              setState(() {
+                if (marked) {
+                  widget._saved.remove(action);
+                } else {
+                  widget._saved.add(action);
+                }
+              });
+            })
+        : IconButton(
+            icon: Icon(Icons.info),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(action.name),
+                      content: Text(action.description),
+                    );
+                  });
+            });
+
+    return ListTile(
+      title: Text(action.name),
+      subtitle: Text(action.description),
+      leading: leading,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String title = widget._selectable ? "Select Actions" : "Available Actions";
+    return WillPopScope(
+        onWillPop: () async {
+          Navigator.pop(context, widget._saved);
+          return false;
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context, widget._saved);
+                },
+              ),
+              title: Text(title),
+            ),
+            body: ListView.builder(
+                itemCount: widget._actions.length,
+                padding: const EdgeInsets.all(16.0),
+                itemBuilder: (context, i) {
+                  return _buildRow(widget._actions.elementAt(i));
+                })));
+  }
+}
+
+class ActionsView extends StatefulWidget {
+  final Set<RemoteAction> _actions = RemoteAction.allActions();
+  Set<RemoteAction> _saved;
+  bool _selectable = false;
+
+  ActionsView({Set<RemoteAction> saved}) {
+    if (saved != null) {
+      _selectable = true;
+      _saved = saved;
+    } else {
+      _saved = Set<RemoteAction>();
+    }
+  }
+
+  @override
+  ActionsViewState createState() => ActionsViewState();
+}
