7c387677ab8576245ddc491bc11ccf7e96e87822
Samuel Albrecht <elsamuko@gmail.com>
2020-03-05 18:30:13 +0100

    add: RemoteAction class

---
+++ lib/remote_action.dart
@@ -0,0 +1,47 @@
+typedef Filter = ActionStatus Function(String stdout);
+
+enum ActionStatus { Success, Warning, Error, Unknown }
+
+class RemoteAction {
+  String name;
+  String description;
+  List<String> commands = [];
+  Filter filter;
+  RemoteAction(this.name);
+
+  static Set<RemoteAction> allActions() {
+    return Set.from([
+      RemoteAction.getDiskFreeAction(),
+    ]);
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return this.name == other.name;
+  }
+
+  @override
+  int get hashCode => name.hashCode;
+
+  RemoteAction.getDiskFreeAction() {
+    name = "df";
+    description = "checks free disk space on /";
+    commands.add("df /");
+    filter = (stdout) {
+      RegExp regExp = new RegExp("(\\d+)%");
+      RegExpMatch match = regExp.firstMatch(stdout);
+
+      if (match.groupCount != 1) return ActionStatus.Unknown;
+
+      int percent = int.tryParse(match[1]);
+
+      if (percent == null) return ActionStatus.Unknown;
+
+      if (percent < 50) return ActionStatus.Success;
+
+      if (percent < 80) return ActionStatus.Warning;
+
+      return ActionStatus.Error;
+    };
+  }
+}
