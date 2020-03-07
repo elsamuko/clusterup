5d6b428ab77ad971d7c88dce6ffd6da75b6959d4
Samuel Albrecht <elsamuko@gmail.com>
2020-03-07 19:11:52 +0100

    add: RemoteActionRunner class + unit test

---
+++ lib/remote_action_runner.dart
@@ -0,0 +1,26 @@
+import 'package:clusterup/cluster.dart';
+import 'package:clusterup/remote_action.dart';
+import 'package:clusterup/ssh_connection.dart';
+import 'package:clusterup/ssh_key.dart';
+
+class RemoteActionRunnerResult {
+  RemoteActionStatus remoteActionStatus;
+  SSHConnectionResult _sshConnectionResult;
+}
+
+class RemoteActionRunner {
+  Cluster _cluster;
+  RemoteAction _action;
+  SSHKey _sshKey;
+
+  RemoteActionRunner(this._cluster, this._action, this._sshKey);
+
+  Future<RemoteActionRunnerResult> run() async {
+    RemoteActionRunnerResult result = RemoteActionRunnerResult();
+    result._sshConnectionResult =
+        await SSHConnection.run(_cluster, _sshKey, _action.commands);
+    result.remoteActionStatus =
+        _action.filter(result._sshConnectionResult.output.join("\n"));
+    return result;
+  }
+}
---
+++ test/remote_action_runner_test.dart
@@ -0,0 +1,25 @@
+import 'package:clusterup/cluster.dart';
+import 'package:clusterup/persistence.dart';
+import 'package:clusterup/remote_action.dart';
+import 'package:clusterup/remote_action_runner.dart';
+import 'package:clusterup/ssh_key.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+// must be run on device for method getDatabasesPath
+// MissingPluginException: https://github.com/tekartik/sqflite/issues/49
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('RunCommands', () async {
+    var db = Persistence();
+    List<Cluster> read = await db.readClusters();
+    SSHKey key = await db.getSSHKey();
+    Cluster cluster = read.first;
+    RemoteAction action = RemoteAction.getDiskFreeAction();
+
+    RemoteActionRunner runner = RemoteActionRunner(cluster, action, key);
+    RemoteActionRunnerResult result = await runner.run();
+
+    expect(result.remoteActionStatus, RemoteActionStatus.Success);
+  });
+}
