d44a5715242a7c62eab83106f5d8d206557b566b
Samuel Albrecht <elsamuko@gmail.com>
2020-03-31 00:25:51 +0200

    add: unit test for server

---
+++ test/server_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:clusterup/server.dart';
+import 'dart:io';
+
+void main() {
+  test('Running server', () async {
+    File f = await File("tmp/web/index.html").create(recursive: true);
+    f.writeAsStringSync("OK");
+
+    Server server = Server();
+    server.start('tmp/web');
+
+    sleep(Duration(milliseconds: 1000));
+
+    String result = await Http.GET('http://localhost:3001');
+    expect(result, "OK");
+
+    server.stop();
+  });
+}
