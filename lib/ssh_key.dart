6604a7a707b8768b8b488b657cfd5b4ec4444838
Samuel Albrecht <elsamuko@gmail.com>
2020-04-20 22:17:14 +0200

    add: operator== for SSHKey

--- lib/ssh_key.dart
+++ lib/ssh_key.dart
@@ -15,11 +15,23 @@ class SSHKey {
     _publicKey = RSAPublicKey(_privateKey.modulus, publicExponent);
   }
 
+  @override
+  bool operator ==(dynamic other) {
+    return this._privateKey == other._privateKey;
+  }
+
+  @override
+  int get hashCode => _privateKey.hashCode;
+
   Map<String, dynamic> toMap() {
     return {
       'id': "ssh_key",
       'private': privString(),
     };
+  }
+
+  Map<String, dynamic> toJson() {
+    return toMap();
   }
 
   // https://github.com/PointyCastle/pointycastle/blob/master/tutorials/rsa.md
