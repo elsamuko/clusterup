85261983d666df150ebe6da683668619ddf1804c
Samuel Albrecht <elsamuko@gmail.com>
2020-05-01 11:52:54 +0200

    chg: cleanup folder for server unit test

--- test/server_test.dart
+++ test/server_test.dart
@@ -4,17 +4,22 @@ import 'dart:io';
 
 void main() {
   test('Running server', () async {
-    File f = await File("tmp/web/index.html").create(recursive: true);
+    String where = "tmp_web";
+
+    File f = await File("$where/index.html").create(recursive: true);
     f.writeAsStringSync("OK");
 
     Server server = Server(3002);
-    server.start('tmp/web');
+    server.start(where);
 
     sleep(Duration(milliseconds: 1000));
 
     String result = await Http.GET('http://localhost:3002');
     expect(result, "OK");
 
-    server.stop();
+    await server.stop();
+
+    // clean up
+    Directory(where).delete(recursive: true);
   });
 }
