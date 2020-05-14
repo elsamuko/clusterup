187efde21616a8d519f5c728ddc6b52c9448cc2e
Samuel Albrecht <elsamuko@gmail.com>
2020-05-14 21:54:56 +0200

    add: deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+GIT_TAG=$(git describe --abbrev=0)
+
+function doPrepare {
+  echo "Clean"
+  flutter clean
+}
+
+function createBundle {
+  echo "Create bundle"
+  flutter build appbundle
+  cp "build/app/outputs/bundle/release/app-release.aab" "tmp/clusterup-$GIT_TAG.aab"
+}
+
+function createAPKs {
+  echo "Create apks"
+  flutter build apk --no-shrink --split-per-abi
+  cp "build/app/outputs/apk/release/app-arm64-v8a-release.apk" "tmp/clusterup-arm64-v8a-$GIT_TAG.apk"
+  cp "build/app/outputs/apk/release/app-armeabi-v7a-release.apk" "tmp/clusterup-armeabi-v7a-$GIT_TAG.apk"
+}
+
+doPrepare
+# createBundle
+createAPKs
