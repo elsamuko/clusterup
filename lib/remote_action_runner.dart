1ae30fbb48a702ef36616bfb9e8f0c89a65c9cea
Samuel Albrecht <elsamuko@gmail.com>
2020-06-06 11:34:46 +0200

    add: RemoteActionRunner.runChildren()

--- lib/remote_action_runner.dart
+++ lib/remote_action_runner.dart
@@ -1,4 +1,5 @@
 import 'package:clusterup/cluster.dart';
+import 'package:clusterup/cluster_child.dart';
 import 'package:clusterup/remote_action.dart';
 import 'package:clusterup/ssh_connection.dart';
 import 'package:clusterup/ssh_key.dart';
@@ -20,5 +21,18 @@ class RemoteActionRunner {
     result._sshConnectionResult = await SSHConnection.run(_cluster.creds(), _sshKey, _action.commands);
     result.remoteActionStatus = _action.filter(result._sshConnectionResult.output);
     return result;
+  }
+
+  Future<List<RemoteActionRunnerResult>> runChildren() async {
+    List<RemoteActionRunnerResult> results = [];
+
+    for (ClusterChild child in _cluster.children) {
+      RemoteActionRunnerResult result = RemoteActionRunnerResult();
+      result._sshConnectionResult = await SSHConnection.run(child.creds(), _sshKey, _action.commands);
+      result.remoteActionStatus = _action.filter(result._sshConnectionResult.output);
+      results.add(result);
+    }
+
+    return results;
   }
 }
