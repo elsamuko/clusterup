08e1df90911ca2ea43bf28785755d48462ae72b9
Samuel Albrecht <elsamuko@gmail.com>
2020-06-11 13:14:29 +0200

    chg: let RemoteActionRunner return a RemoteActionResult

--- lib/remote_action_runner.dart
+++ lib/remote_action_runner.dart
@@ -4,23 +4,18 @@ import 'package:clusterup/remote_action.dart';
 import 'package:clusterup/ssh_connection.dart';
 import 'package:clusterup/ssh_key.dart';
 
-class RemoteActionRunnerResult {
-  RemoteActionStatus remoteActionStatus;
-  SSHConnectionResult _sshConnectionResult;
-}
-
 class RemoteActionRunner {
-  SSHCredentials _creds;
-  RemoteAction _action;
-  SSHKey _sshKey;
+  final SSHCredentials _creds;
+  final RemoteAction _action;
+  final SSHKey _sshKey;
 
   RemoteActionRunner(this._creds, this._action, this._sshKey);
 
-  Future<RemoteActionRunnerResult> run() async {
-    RemoteActionRunnerResult result = RemoteActionRunnerResult();
-    result._sshConnectionResult = await SSHConnection.run(_creds, _sshKey, _action.commands);
-    if (result._sshConnectionResult.success) {
-      result.remoteActionStatus = _action.filter(result._sshConnectionResult.output);
+  Future<RemoteActionResult> run() async {
+    RemoteActionResult result;
+    SSHConnectionResult sshConnectionResult = await SSHConnection.run(_creds, _sshKey, _action.commands);
+    if (sshConnectionResult.success) {
+      result = _action.filter(sshConnectionResult.output);
     }
     return result;
   }
