60173d24439a8ddb7cf35e0dab280dd4e3273d18
Samuel Albrecht <elsamuko@gmail.com>
2020-06-19 10:54:35 +0200

    add: unit test for CPU load action

--- test/remote_action_test.dart
+++ test/remote_action_test.dart
@@ -50,4 +50,28 @@ void main() {
     RemoteActionResult result = action.filter(["4.19.66-v7+"]);
     expect(result.status, RemoteActionStatus.Success);
   });
+
+  test('test action CPU load', () {
+    // from a idle raspberry
+    RemoteAction action = RemoteAction.getCPULoadAction();
+    RemoteActionResult result = action.filter([
+      "cpu  767507 9892 329507 1257115062 63614 0 4859 0 0 0",
+      "procs_blocked 0",
+      "softirq 200284061 8771478 69993392 652986 1139028 0 0 5258061 69446212 358 45022546",
+      "cpu  767507 9892 329509 1257115466 63614 0 4859 0 0 0",
+      "ctxt 163930158",
+    ]);
+    expect(result.status, RemoteActionStatus.Success);
+
+    // sysbench --num-threads=3 --test=cpu --cpu-max-prime=20000 run
+    result = action.filter([
+      "cpu  769737 9892 329610 1257431317 63622 0 4862 0 0 0",
+      "cpu0 222527 2144 81383 310606580 19261 0 4822 0 0 0",
+      "procs_blocked 0",
+      "softirq 200339971 8773527 70013401 653153 1139366 0 0 5259474 69464596 358 45036096",
+      "cpu  770040 9892 329611 1257431416 63622 0 4862 0 0 0",
+      "ctxt 163970668",
+    ]);
+    expect(result.status, RemoteActionStatus.Warning);
+  });
 }
