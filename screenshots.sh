0ab6339c26509ccc0d169d35a0f5b962ceb3db97
Samuel Albrecht <elsamuko@gmail.com>
2020-07-03 13:16:42 +0200

    add: title to screenshots

--- screenshots.sh
+++ screenshots.sh
@@ -5,3 +5,16 @@ adb forward tcp:3001 tcp:3001
 
 # take screenshots
 flutter drive --target=test_driver/screenshot.dart
+
+cd screenshots || exit
+
+SHOTS=( add_server.png
+        edit_server.png
+        load_save.png
+        run.png
+        view_key.png )
+
+for SHOT in "${SHOTS[@]}"; do
+  echo "Adding title to $SHOT"
+  convert "$SHOT" -page +0+0 ../res/title.png -flatten "$SHOT"
+done
