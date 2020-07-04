27ca5a6bebd361c3065d374b6c0d933efe95b4a6
Samuel Albrecht <elsamuko@gmail.com>
2020-07-04 16:51:45 +0200

    chgL let ClusterUpData.fromJSON parse plain RSA private keys, too

--- lib/clusterup_data.dart
+++ lib/clusterup_data.dart
@@ -34,14 +34,19 @@ class ClusterUpData {
 
   static ClusterUpData fromJSON(String input) {
     ClusterUpData output = ClusterUpData();
+
     Map<String, dynamic> data;
+
     try {
       data = jsonDecode(input);
     } on FormatException catch (e) {
-      log("json parse error ${e.toString()}");
+      log("json parse error $e");
     }
 
+    // parse as json
     if (data != null) {
+      log("upload is json");
+
       if (data.containsKey("clusters")) {
         data["clusters"].forEach((clusterData) {
           output.clusters.add(Cluster.fromMap(clusterData));
@@ -50,6 +55,15 @@ class ClusterUpData {
 
       if (data.containsKey("key")) {
         output.sshKey = SSHKey.fromPEM(data["key"]["private"]);
+      }
+    }
+
+    // parse as private key
+    else if (input.startsWith("-----BEGIN PRIVATE KEY-----") || input.startsWith("-----BEGIN RSA PRIVATE KEY-----")) {
+      log("upload is private key");
+      SSHKey key = SSHKey.fromPEM(input);
+      if (key != null) {
+        output.sshKey = key;
       }
     }
 
