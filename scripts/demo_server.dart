bf1313a6816da627fc5baa6ef895e8ab2d6eb748
Samuel Albrecht <elsamuko@gmail.com>
2020-07-10 17:00:06 +0200

    chg: set onJsonOrKey for demo_server

--- scripts/demo_server.dart
+++ scripts/demo_server.dart
@@ -3,6 +3,7 @@ import 'package:clusterup/server.dart';
 
 int main() {
   Server server = Server(3003);
+  server.onJsonOrKey = (json) { server.json = json; };
   server.start('res/web');
   return 0;
 }
