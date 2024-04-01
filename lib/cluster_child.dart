ca47c0e9ddd1e575eb4db97907440d6bd21237a2
Samuel Albrecht <elsamuko@gmail.com>
2024-04-01 22:10:59 +0200

    add: password to clusterchild

--- lib/cluster_child.dart
+++ lib/cluster_child.dart
@@ -7,13 +7,15 @@ class ClusterChild {
   int? id;
   String? user;
   String? host;
+  String? password;
   int? port;
   bool enabled;
 
   // runtime only
   bool up = false; // true, if valid and reachable
 
-  ClusterChild(this.parent, {this.user, this.host, this.port, this.enabled = true}) {
+  ClusterChild(this.parent,
+      {this.user, this.host, this.password, this.port, this.enabled = true}) {
     id = parent.children.length;
   }
 
@@ -26,12 +28,18 @@ class ClusterChild {
     if (this.id != other.id) return false;
     if (this.user != other.user) return false;
     if (this.host != other.host) return false;
+    if (this.password != other.password) return false;
     if (this.port != other.port) return false;
     return true;
   }
 
   @override
-  int get hashCode => id.hashCode ^ user.hashCode ^ host.hashCode ^ port.hashCode;
+  int get hashCode =>
+      id.hashCode ^
+      user.hashCode ^
+      host.hashCode ^
+      password.hashCode ^
+      port.hashCode;
 
   Map<String, dynamic> toMap() {
     return {
@@ -39,6 +47,7 @@ class ClusterChild {
       'id': id,
       'user': user,
       'host': host,
+      'password': password,
       'port': port,
       'enabled': enabled ? 1 : 0,
     };
@@ -51,12 +60,14 @@ class ClusterChild {
       parent,
       user: data['user'],
       host: data['host'],
+      password: data['password'],
       port: data['port'],
       enabled: (data['enabled'] ?? 1) == 1,
     );
   }
 
   SSHCredentials creds() {
-    return SSHCredentials(user ?? parent.user, host ?? parent.host, port ?? parent.port);
+    return SSHCredentials(user ?? parent.user, host ?? parent.host,
+        password ?? parent.password, port ?? parent.port);
   }
 }
