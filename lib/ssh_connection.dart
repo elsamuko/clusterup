5c91b6b8a6b14a943cedd49fc7d55df3c8b954ff
Samuel Albrecht <elsamuko@gmail.com>
2025-06-15 19:23:43 +0200

    chg: replaced ssh2 with dartssh2

--- lib/ssh_connection.dart
+++ lib/ssh_connection.dart
@@ -1,8 +1,9 @@
 import 'dart:core';
 import 'package:clusterup/log.dart';
 import 'package:clusterup/ssh_key.dart';
-import 'package:ssh2/ssh2.dart';
+import 'package:dartssh2/dartssh2.dart';
 import 'package:flutter/services.dart';
+import 'dart:convert';
 
 class SSHConnectionResult {
   bool success;
@@ -34,36 +35,28 @@ class SSHConnection {
 
   static Future<SSHConnectionResult> run(SSHCredentials creds, SSHKey key, List<String> commands) async {
     SSHConnectionResult rv = SSHConnectionResult(false, creds);
-    SSHClient client = SSHClient(
-      host: creds.host,
-      port: creds.port,
-      username: creds.user,
-      passwordOrKey: creds.password ?? {"privateKey": key.privString()},
+    final socket = await SSHSocket.connect(
+      creds.host,
+      creds.port,
     );
-    log("trying to connect to $creds");
-    try {
-      String? result = await client.connect();
-      if (result == "session_connected") {
-        log("connected to $creds");
-        for (String command in commands) {
-          log("Running $command");
-          String? out = await client.execute(command);
-          if (out != null && out.isNotEmpty) {
-            log("Got $out");
-            rv.output += out.split("\r\n");
-            if (rv.output.last.isEmpty) {
-              rv.output.removeLast();
-            }
-          }
+    final client = SSHClient(
+      socket,
+      username: creds.user,
+      onPasswordRequest: () => creds.password,
+      identities: SSHKeyPair.fromPem(key.privString()),
+    );
+    for (String command in commands) {
+      log("Running $command");
+      String out = utf8.decode(await client.run(command));
+      if (out.isNotEmpty) {
+        log("Got $out");
+        rv.output += out.split("\n");
+        if (rv.output.last.isEmpty) {
+          rv.output.removeLast();
         }
       }
-      client.disconnect();
-      log("disconnected from $creds");
-      rv.success = true;
-    } on PlatformException catch (e) {
-      print('Error: ${e.code}\nError Message: ${e.message}');
-      rv.error = e.message ?? "SSHConnection.run()";
     }
+    rv.success = true;
     return rv;
   }
 }
